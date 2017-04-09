19e1f0d6f31d5d626bf58a93bfac256ee755ced1
helaliFerid <helaliferid@gmai.com>
2017-04-09 15:24:44 +0100

    install ng2-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,12 @@
+import { RatingModule } from 'ng2-bootstrap/rating';
+import { PaginationModule } from 'ng2-bootstrap/pagination';
+import { ModalModule } from 'ng2-bootstrap/modal';
+import { BsDropdownModule } from 'ng2-bootstrap/dropdown';
+import { CollapseModule } from 'ng2-bootstrap/collapse';
+import { CarouselModule } from 'ng2-bootstrap/carousel';
+import { ButtonsModule } from 'ng2-bootstrap/buttons';
+import { AccordionModule } from 'ng2-bootstrap/accordion';
+import { AlertModule } from 'ng2-bootstrap/alert';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -5,16 +14,29 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    AccordionModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+    RatingModule.forRoot()
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
