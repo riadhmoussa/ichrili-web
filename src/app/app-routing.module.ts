e20e3fb3fe04222808dec9a2c23f4de92eb6be6e
feridHelali <helaliferid@gmail.com>
2017-04-17 16:26:29 +0100

    NavbarModule and component mouved to module folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,21 @@ import { LoginComponent } from './ui/login/login.component';
 import { HomeComponent } from './containers/home/home.component';
 import { AuthGuard } from './guards/auth.guards';
 import { RegisterComponent } from './register/register.component';
+import { UpdateSettingsComponent } from './modules/update-settings/update-settings.component';
+import { NavabarModule } from './modules/navabar/navabar.module';
 
 const routes: Routes = [
   {
     path: 'login', component: LoginComponent
   },
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'user/settings', component: UpdateSettingsComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),NavabarModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
