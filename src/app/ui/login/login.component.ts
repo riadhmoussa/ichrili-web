3c5b19b4c139fa79bd091fda542decc859ce6174
helaliFerid <helaliferid@gmai.com>
2017-06-28 09:47:26 +0100

    Reprise

--- src/app/ui/login/login.component.ts
+++ src/app/ui/login/login.component.ts
@@ -3,42 +3,43 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
 import { AlertService } from '../../services/alert.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
-  model: any = {};
-  loading = false;
-  returnUrl: String;
+    model: any = {};
+    loading = false;
+    returnUrl: String;
 
-  constructor(
-    private route: ActivatedRoute,
+    constructor(
+        private route: ActivatedRoute,
         private router: Router,
         private authenticationService: AuthenticationService,
         private alertService: AlertService) { }
 
-  ngOnInit() {
-     // reset login status
+    ngOnInit() {
+        // reset login status
         this.authenticationService.logout();
         // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-  }
-
-  login() {
-   this.loading = true;
-        this.authenticationService.login(this.model.username, this.model.password)
-            .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
-                },
-                error => {
-                    this.alertService.error(error._body);
-                    this.loading = false;
-                });
     }
 
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                this.alertService.error(error._body);
+                this.loading = false;
+            });
+    }
 }
